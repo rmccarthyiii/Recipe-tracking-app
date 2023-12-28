d9c0dcd95b4f95518c67ff0603189213f2930459
Robert McCarthy <romccarthy@indeed.com>
2023-12-28 02:02:31 -0600

    Updates

---
+++ precache-manifest.8c823fe1785943e407cbde6fad23e1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6b9273f0802449752d3865241a8936",
+    "url": "/rmccarthyiii/Recipe-tracking-app/index.html"
+  },
+  {
+    "revision": "4f17353ad1845f2a6dda",
+    "url": "/rmccarthyiii/Recipe-tracking-app/static/css/main.f90b42b0.chunk.css"
+  },
+  {
+    "revision": "dbf19fd96ea7ed36de7a",
+    "url": "/rmccarthyiii/Recipe-tracking-app/static/js/2.e1e0ccdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rmccarthyiii/Recipe-tracking-app/static/js/2.e1e0ccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f17353ad1845f2a6dda",
+    "url": "/rmccarthyiii/Recipe-tracking-app/static/js/main.63454775.chunk.js"
+  },
+  {
+    "revision": "2ddcef21383d7754659e",
+    "url": "/rmccarthyiii/Recipe-tracking-app/static/js/runtime-main.9d5d0edc.js"
+  }
+]);
